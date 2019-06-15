f4da0514ddb17becb166ae6d3e9dd6946838e5b1
solitas <solitasroh@gmail.com>
2019-06-15 11:03:29 +0900

    Initial Commit

---
+++ AppSample/Sample01/ComObjectHelper.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+using System;
+using System.IO;
+
+using ExcelApp = Microsoft.Office.Interop.Excel.Application;
+
+namespace Sample01
+{
+    public static class ComObjectHelper
+    {
+        public static void ReadExcelFromFile(string filePath)
+        {
+            if (!IsValidFile(filePath))
+            {
+                Console.WriteLine("You tried to open a invalid file!!");
+                return;
+            }
+
+            ExcelApp xlApp;
+            Workbook xlWorkBook;
+            Worksheet xlWorkSheet;
+            Range range;
+
+            string str;
+            int rowCount = 0; // 열 갯수
+            int columnCount = 0; // 행 갯수
+
+            xlApp = new ExcelApp();
+            xlWorkBook = xlApp.Workbooks.Open(filePath, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Worksheet)xlWorkBook.Worksheets.get_Item(1); // 첫번째 시트를 가져 옴.
+
+            range = xlWorkSheet.UsedRange; // 가져 온 시트의 데이터 범위 값
+
+            for (rowCount = 1; rowCount <= range.Rows.Count; rowCount++)
+            {
+                var cell = range.Cells[rowCount, columnCount] as Range;
+
+                for (columnCount = 1; columnCount <= range.Columns.Count; columnCount++)
+                {
+                    str = (string)(range.Cells[rowCount, columnCount] as Range).Value2; // 열과 행에 해당하는 데이터를 문자열로 반환
+                    Console.WriteLine(str);
+                }
+            }
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            ReleaseObject(xlWorkSheet);
+            ReleaseObject(xlWorkBook);
+            ReleaseObject(xlApp);
+        }
+        private static bool IsValidFile(string filePath)
+        {
+            FileInfo info = new FileInfo(filePath);
+            try
+            {
+                if (info.Exists)
+                {
+                    if (info.Extension == ".xlsx" || info.Extension == ".xls")
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private static void ReleaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                Console.WriteLine("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
---
+++ AppSample/Sample01/Model/Tag.cs
@@ -0,0 +1,71 @@
+﻿using ReactiveUI.Fody.Helpers;
+
+namespace Sample01.Model
+{
+    public interface ITag
+    {
+        string Name { get; set; }
+    }
+
+    public class IncomingTag : ITag
+    {
+        [Reactive]
+        public string Name { get; set; }
+        /// <summary>
+        /// 01 : Local  A2700M
+        /// 02 : Remote  A2700M
+        /// 03 : A2750P
+        /// 04 : A2750PC
+        /// </summary>
+        [Reactive]
+        public ushort DeviceType { get; set; }
+
+        /// <summary>
+        /// 01 : Memory         (Only A2700M)
+        /// 02 : Remote         (Only Remote)
+        /// 03 : DI             (Only A2750P,PC)
+        /// 04 : DO             (Only A2750P,PC)
+        /// 05 : MOTOR          (Only A2750P,PC)
+        /// 06 : Logic Control  (Only A2750P,PC)
+        /// 07 : Logic Status   (Only A2750P,PC)
+        /// </summary>
+        [Reactive]
+        public Channel Channel { get; set; }
+        /// <summary>
+        /// 01 ~ 40
+        /// </summary>
+        [Reactive]
+        public ushort ChannelId { get; set; }
+        /// <summary>
+        /// 01 : BOOL
+        /// 02 : DINT
+        /// 03 : UDINT
+        /// 04 : REAL
+        /// 05 : TIME
+        /// </summary>
+        [Reactive]
+        public ElementaryDataType DataType { get; set; }
+    }
+
+    public enum Channel
+    {
+        None,
+        Memory,
+        Remote,
+        DI,
+        DO,
+        MOTOR,
+        LogicControl,
+        LogicStatus,
+    }
+
+    public enum ElementaryDataType
+    {
+        None    /**/ = 0x00,
+        Bool    /**/ = 0x01,
+        Dint    /**/ = 0x02,
+        Udint   /**/ = 0x03,
+        Real    /**/ = 0x04,
+        Time    /**/ = 0x05
+    }
+}
